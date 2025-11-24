fc2719764c8a31c79941636de4833a95976077e9
Wilt <simpsonwilliam948@gmail.com>
2025-11-23 17:39:37 -0700

    Simple implementation of MCTS loop

--- orbital/DEC_MCTS.py
+++ orbital/DEC_MCTS.py
@@ -12,12 +12,87 @@ from skyfield.api import load
 import json
 import warnings
 import math
+import random
+
+
 
 warnings.filterwarnings("ignore", category=RuntimeWarning)
 
 # Constants
 WORST_CASE_SLEW_PER_ACTION = np.pi
+MAX_DEPTH = 20 # SMW : Adjust plannign horizion 
 
+
+class MCTSNode:
+    def __init__(self, state = None, parent=None, gamma=0.99, depth = 0):
+        self.state = state
+        self.parent = parent
+        self.children = {}
+
+        # Discount factor 
+        self.gamma = gamma
+
+        #Exploration Constant
+        self.c = 0.1
+
+        # Discounted statistics
+        self.disc_visits = 0.0        # C_t(s)
+        self.disc_value = 0.0         # discounted return estimate
+
+        # For standard MCTS if needed
+        self.visits = 0
+        self.value = 0.0
+
+        # Depth and terminal state if we force a terminal statet
+        self.depth = depth 
+        self.is_terminal = False
+    
+    def decay(self): # this should be called every global timestep even if the node is not visited
+        # Apply a decay when the node is not visited 
+        self.disc_visits *= self.gamma # C_t(s)
+        self.disc_value *= self.gamma # sum
+    
+    def update_visit(self, reward):
+        # Apply the discounted update when we visit the node
+        self.disc_visits = self.gamma * self.disc_visits + 1
+        self.disc_value = self.gamma * self.disc_value + reward
+        self.visits += 1
+        self.value += reward
+    
+    def d_uct(self, parent):
+        # Safety: avoid divide by 0 early in search
+        if parent.disc_visits == 0 or self.disc_visits == 0:
+            return 1e4 # High number since we want to ensure unvisited nodes are prioritized to be visited 
+
+        exploitation = self.disc_value / self.disc_visits
+        parent_visits_safe = max(parent.disc_visits, 1.0) # Clamp input for log to be at least 1
+        exploration = self.c * math.sqrt(
+            math.log(parent_visits_safe) / self.disc_visits
+        )
+
+        return exploitation + exploration
+    def best_child(self):
+        # Since we handle the children for each node we can iterate from this function and find the max
+        if not self.children:
+            return None
+        return max(self.children.values(), key = lambda child: child.d_uct(self))
+    
+    def add_child(self, action, next_state):
+        node = MCTSNode(
+            state=next_state, 
+            parent = self, 
+            gamma = self.gamma,
+            depth=self.depth + 1
+        )
+        self.children[action] = node;
+        return node 
+    
+    def check_terminal(self):
+        if self.depth >= MAX_DEPTH:
+            self.is_terminal = True
+            return True
+        return False
+    
 class Observer:
     def __init__(self, host, host_ind):
         self.host = host
@@ -59,57 +134,82 @@ class Observer:
     def save_maps(self, fname):
         np.savez(fname, np.dstack(self.maps))
 
-class MCTSNode:
-    def __init__(self, state, parent=None, gamma=0.99):
-        self.state = state
-        self.parent = parent
-        self.children = {}
+    # SMW added functions for selecting/expanding tree
+    def select_node(self):
+        # So traverse the tree using DUCT until we reach a leaf node
+        node = self.root
+        path = [node]
 
-        # Discount factor 
-        self.gamma = gamma
-
-        #Exploration Constant
-        self.c = 1
-
-        # Discounted statistics
-        self.disc_visits = 0.0        # C_t(s)
-        self.disc_value = 0.0         # discounted return estimate
-
-        # For standard MCTS if needed
-        self.visits = 0
-        self.value = 0.0
+        while node.children and not node.is_terminal:
+            child = node.best_child()
+            if child is None:
+                break
+            node = child
+            path.append(node)
+        
+        return node, path 
     
-    def decay(self): # this should be called every global timestep even if the node is not visited
-        # Apply a decay when the node is not visited 
-        self.disc_visits *= self.gamma # C_t(s)
-        self.disc_value *= self.gamma # sum
+    def expand(self, node, num_children = 5):
+        # Expand by creating random next states (right now I set to 5 default )
+
+        node.check_terminal()
+
+        if node.is_terminal:
+            return None # Reach terminal state 
+        
+        for a in range(num_children): # Static node width so just make num_children amount of state action pairs 
+            next_state = np.random.randint(0,100) # SMW:  Replace this with actual state 
+            node.add_child(a,next_state).depth= node.depth + 1
+        # From the nodes we created randomly select one to work from 
+        return random.choice(list(node.children.values()))
     
-    def update_visit(self, reward):
-        # Apply the discounted update when we visit the node
-        self.disc_visits = self.gamma * self.disc_visits + 1
-        self.disc_value = self.gamma * self.disc_value + reward
-        self.visits += 1
-        self.value += reward
+    def simulate(self,node):
+        # Dummy rollout
+
+        if node.is_terminal:
+            return 0.0
+        
+        return np.random.uniform(-1, 1)
     
-    def d_uct(self, parent):
-        # Safety: avoid divide by 0 early in search
-        if parent.disc_visits == 0 or self.disc_visits == 0:
-            return float("inf")
+    def backpropagate(self, path, reward):
+        # Discounted backip each step we apply a discount
+        for node in reversed(path):
+            node.update_visit(reward)
+            if node.parent is None:
+                continue
+            node.disc_visits *= node.gamma
+            node.disc_value  *= node.gamma
+    def mcts_iter(self):
+        # 1. Selection
+        leaf,path = self.select_node()
 
-        exploitation = self.disc_value / parent.disc_visits
-        exploration = self.c * math.sqrt(
-            math.log(self.disc_visits) / parent.disc_visits
-        )
+        # 2. Expansion
+        if not leaf.children:
+            leaf = self.expand(leaf)
+            path.append(leaf)
+        
+        # 3. Simulate
+        reward = self.simulate(leaf)
 
-        return exploitation + exploration
+        # 4. backpropagate 
+        self.backpropagate(path, reward)
 
+        return reward
+    
+    
 if __name__=="__main__":
-    sats = load_satellites()
+    obs = Observer(host = None, host_ind=0)
 
-    import time
-    start = time.perf_counter()
+    for i in range(1000):
+        r = obs.mcts_iter()
+        print(f"Iter {i}: reward={r:.3f}")
+
+    #sats = load_satellites()
+
+    #import time
+    #start = time.perf_counter()
 
     # Select a set of hosts and make targets a view of the rest of the stuff in that list of satellites
-    hosts = sats[0:4]
-    targets = sats[4:] # Technically this is incorrect, as each telescope should look at the other hosts too!!!
+    #hosts = sats[0:4]
+    #targets = sats[4:] # Technically this is incorrect, as each telescope should look at the other hosts too!!!
 
